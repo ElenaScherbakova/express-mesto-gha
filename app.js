f7777f22f55445ed8c8a7f5833876afa8179865a
Elena Shcherbakova <25pochta25@mail.ru>
2023-03-06 16:42:40 +0300

    исправление ошибок

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const cors = require("cors")
 const express = require("express")
 const userRouter = require("./routes/user-router")
 const cardsRouter = require("./routes/cards-router")
-const User = require("./models/user")
 const { http404 } = require("./controllers/http-responses");
 
 const { PORT = 3000 } = process.env
 
 connect("mongodb://127.0.0.1:27017/mestodb", {})
-  .then(() => User.findOne({}))
-  .then((user) => {
+  .then(() => {
     const app = express();
     app.use(cors())
     app.use(express.json())
     app.use((req, res, next) => {
-      req.user = user
+      req.user = {
+        _id: "5d8b8592978f8bd833ca8133",
+      }
       next()
     })
     app.use("/users", userRouter)
